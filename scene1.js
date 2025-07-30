262901945c2819da8f9c809ec5d5c67de2b6005c
Jack Braun <j.braun3456@gmail.com>
2025-07-30 14:34:24 -0400

    Created the ship and added the animation for it.

--- scene1.js
+++ scene1.js
@@ -5,9 +5,21 @@ class scene1 extends Phaser.Scene {
 
 preload() {
         this.load.image("background", "assets/background.png");
+
+        this.load.spritesheet("ship", "assets/ship.png", {
+            frameWidth: 45,
+            frameHeight: 44
+        });
     }
 
 create() {
     this.scene.start("playGame");
+
+    this.anims.create({
+    key: "ship_animation",
+    frames: this.anims.generateFrameNumbers("ship", {start:0, end: 13}),
+    frameRate: 20,
+    repeat: -1
+    })
 }
 }
--- scene2.js
+++ scene2.js
@@ -6,6 +6,9 @@ class scene2 extends Phaser.Scene {
 create(){
     this.background = this.add.tileSprite(0,0, config.width, config.height, "background");
     this.background.setOrigin(0,0);
+
+    this.ship = this.add.sprite(config.width/2, config.height/2 + 200, "ship");
+    this.ship.play("ship_animation");
     }
 
 update(){
